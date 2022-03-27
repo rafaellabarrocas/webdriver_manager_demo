701071e517e882dc87351df6431a891ee18cfc23
Rafaella Barrocas <rafaellabarrocas@Rafaellas-MacBook-Pro.local>
2022-03-27 15:16:24 -0300

    updated google search

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -4,4 +4,4 @@ from pages.search_page import SearchPage
 class TestGoogleSearch(BaseTest):
     def test_search_squirt(self):
         page = SearchPage(self.driver)
-        page.googleSearch("Rafaella Barrocas Linkedin")
+        page.googleSearch("Rafaella Barrocas Senior QA Engineer Linkedin")
